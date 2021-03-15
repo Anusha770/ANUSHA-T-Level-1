af90a1838b079880b64f73c9b6c22bc9b47c5ec4
Anusha770 <75679265+Anusha770@users.noreply.github.com>
2021-03-15 21:30:09 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,59 +1,61 @@
 //WAP to find the sum of two fractions.
 #include <stdio.h>
-#include <math.h>
+struct point
+{
+  int n, d;
+};
 
+int
+greatestcommondivisor (int a, int b)
+{
+  int gcd = 1, i;
 
-    struct point
+  for (i = 1; i <= a && i <= b; i++)
     {
-       int n,d;
-    };
-    
-    int g(int a , int b)
-    {
-    int gcd=1,i=1;
-   
-    while (i<=a&&i<=b)
-    {
-    if(a%i==0&&b%i==0)
-    gcd=i;
-    i++;
+      if (a % i == 0 && b % i == 0)
+	gcd = i;
     }
-    return gcd;
-    }
-     struct point compute(struct point a,struct point b)
-    {
-    struct point sum;
-         int gcd;
-        sum.n=(a.n*b.d)+(b.n*a.d);
-        sum.d=a.d*b.d;
-        gcd=g(sum.n,sum.d);
-        sum.d=(sum.d)/gcd;
-        sum.n=(sum.n)/gcd;
-    return sum;
-    }
-    struct point input()
-    {
-        struct point n;
-        printf("numerator:");
-        scanf("%d",&n.n);
-        printf("denominator:");
-        scanf("%d",&n.d);
-        return n;
-    }
-     
- void out(struct point c)
-    {
-        printf("sum=%d/%d",c.n,c.d);
-    }
+  return gcd;
+}
 
+struct point
+compute (struct point a, struct point b)
+{
+  struct point sum;
+  int gcd;
+  sum.n = (a.n * b.d) + (b.n * a.d);
+  sum.d = a.d * b.d;
+  gcd = greatestcommondivisor (sum.n, sum.d);
+  sum.d = (sum.d) / gcd;
+  sum.n = (sum.n) / gcd;
+  return sum;
+}
 
-    int main()
-    {
-        struct point a,b,c;
-        a=input();
-        b=input();
-        c=compute(a,b);
-       
-        out(c);
-        return 0;
-    }
+struct point
+input ()
+{
+  struct point n;
+  printf ("numerator:");
+  scanf ("%d", &n.n);
+  printf ("denominator:");
+  scanf ("%d", &n.d);
+  return n;
+}
+
+void
+out (struct point a, struct point b, struct point c)
+{
+  printf ("sum of %d/%d and %d/%d is %d/%d", a.n, a.d, b.n, b.d, c.n, c.d);
+}
+
+int
+main ()
+{
+  struct point a, b, c;
+  a = input ();
+  b = input ();
+  c = compute (a, b);
+  out (a, b, c);
+  return 0;
+}
+
